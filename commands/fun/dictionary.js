a54fe8037cf4f19e88e0850578e8216f883468a4
Ducxy <dkidd5151@gmail.com>
2020-08-23 16:57:04 -0400

    Update dictionary.js

--- commands/fun/dictionary.js
+++ commands/fun/dictionary.js
@@ -1,30 +1,32 @@
 const discord = require('discord.js');
 const urban = require('urban');
+const { stripIndents }  = require('common-tags')
 
 module.exports = {
-        name: "dictionary",
+        name: "1dictionary",
         aliases: ["urban", "ud", "dic"],
         category: "Fun",
         description: "Find a word in the Urban Dictionary",
         usage: "$dictionary <word>",
     run: async (bot, message, args) => {
 
-        let icon = "https://i.pinimg.com/originals/04/83/c7/0483c76e02e484dbf20009418758849e.jpg";
-        let search = args[1] ? urban(args.slice(1).join(" ")) : urban.random();
+        if(!args[0]) return message.channel.send('Please specify a word!')
+        let icon = "https://is4-ssl.mzstatic.com/image/thumb/Purple111/v4/7e/49/85/7e498571-a905-d7dc-26c5-33dcc0dc04a8/source/512x512bb.jpg";
+        let search = args[0] ? urban(args.join(" ")) : urban.random();
             try {
                 search.first(res => {
                     if(!res) return message.channel.send("No results found for that word!");
                     let { word, definition, example, thumbs_up, thumbs_down, permalink} = res;
 
-                        let embed = new RichEmbed()
+                        let embed = new discord.MessageEmbed()
                             .setColor('BLACK')
-                            .setTitle(word + 'Urban Dictionary')
+                            .setTitle('"'+word + '" - Urban Dictionary')
                             .setThumbnail(icon)
                             .setDescription(stripIndents`**Defintion:** ${definition || "No definition."}
                             **Example:** ${example || "No example."}
                             **Upvotes:** ${thumbs_up || 0}
                             **Downvotes:** ${thumbs_down || 0}
-                            **Link:** [link to ${word}](${permalink || "https://www.urbandictionary.com/"})`)
+                            **Link:** [${word}](${permalink || "https://www.urbandictionary.com/"})`)
 
                             message.channel.send(embed)
                 })
