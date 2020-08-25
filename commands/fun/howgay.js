b622a4d8e4401e03569977a7754ddc6dffa36185
Ducxy <dkidd5151@gmail.com>
2020-08-24 20:55:06 -0400

    Update howgay.js

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -15,7 +15,6 @@ module.exports = {
             )
           }
     
-        message.channel.send(`<@${target.id}>` + ' is ' + between(1, 100) + '% gay :rainbow_flag:');
         if(!target) return message.channel.send(`You are ` + between(1, 100) + '% gay :rainbow_flag:')
         else { message.channel.send(`<@${target.id}>` + ' is ' + between(1, 100) + '% gay :rainbow_flag:'); }
     }
