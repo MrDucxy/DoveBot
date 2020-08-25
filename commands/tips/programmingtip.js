3ddb9ff50da9a8200c93051c31c4c38a6a4e2d5a
Ducxy <dkidd5151@gmail.com>
2020-08-25 17:47:02 -0400

    ffff

---
+++ commands/tips/programmingtip.js
@@ -0,0 +1,27 @@
+const discord = require('discord.js');
+const replyFile = require('./devTips.json')
+
+module.exports = {
+        name: "programmingtip",
+        aliases: ["devtip", "codetip", "devstip"],
+        category: "Tips",
+        description: "Gives you a random tip for helping out with programming",
+        usage: "$programmingtip",
+    run: async (bot, message, args) => {
+
+        let replies = replyFile.devTips;
+
+        let result = Math.floor((Math.random() * replies.length));
+
+        let embed = new discord.MessageEmbed()
+        .setColor('#007dff')
+        .setThumbnail('https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
+        .setAuthor('Cubic | Tips', 'https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
+        .addField('Programming Tip', replies[result])
+
+        message.reply(embed);
+        
+    }
+
+};
+
