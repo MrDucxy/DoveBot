6f335309b33790a0366bdf6683e1e703424f2507
Ducxy <dkidd5151@gmail.com>
2020-08-26 01:20:15 -0400

    fff

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,5 +1,5 @@
 const discord = require('discord.js');
-var ping = require('ping');
+var pingJS = require('ping-lite');
 
 
 module.exports = {
@@ -12,33 +12,17 @@ module.exports = {
 
         if(!args[0]) return message.channel.send('Pong! | Latency: ' + Math.round(bot.ws.ping) + 'ms')
         else{
-            var hosts = [args[0]];
-            hosts.forEach(function (host) {
-                ping.promise.probe(host)
-                    .then(function (res) {
-
-                        if(res.host != 'unknown'){
-                            let embed = new discord.MessageEmbed()
-                            .setColor('#007dff')
-                            .setTitle('Ping Response: '+res.host)
-                            .addField('Time', `${res.time}ms`)
-                            .addField('Host IP', res.numeric_host)
-                            .setAuthor('Cubic | Ping', 'https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
+            var ping = new pingJS(args[0]);
+            ping.send(function(err, res){
+                let embed = new discord.MessageEmbed()
+                .setColor('#007dff')
+                .setTitle('Ping Response: '+ args[0])
+                .addField('Host:', ping._host)
+                .addField('Time',`${Math.round(res)}ms`)
+                .setAuthor('Cubic | Ping', 'https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
     
-                            message.channel.send(embed)
-                        }
-                        else{
-                            let embed = new discord.MessageEmbed()
-                            .setColor('#007dff')
-                            .setTitle('Ping Response: ')
-                            .setDescription('This host did not reply or timed out.')
-                            .setAuthor('Cubic | Ping', 'https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
-    
-                            message.channel.send(embed)
-                        }
-                        
-                    });
-            });
+                message.channel.send(embed)
+            })
 
         }
 
