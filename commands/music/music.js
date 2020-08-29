6bbfaf618b6a32f0a6a0f19789a143492bc9c9fe
Ducxy <dkidd5151@gmail.com>
2020-08-29 18:05:25 -0400

    ffff

--- commands/music/music.js
+++ commands/music/music.js
@@ -3,76 +3,108 @@ const ytdl = require('ytdl-core');
 const opus = require('opusscript');
 const { connect } = require('superagent');
 
-var servers = {};
+const queue = new Map()
 
 module.exports = {
         name: "music",
-        aliases: ["tunes", "sl"],
+        aliases: ["tunes"],
         category: "Music",
         description: "Play some music in a voice channel!",
-        usage: "$music <song name/link>",
+        usage: "$music <function(play, pause, skip, etc)> <song name/link>",
     run: async (bot, message, args) => {
+        const voiceChannel = message.member.voice.channel;
+        const permissions = voiceChannel.permissionsFor(message.client.user)
 
-        if(!message.member.voice) return message.channel.send('You must be in a voice channel to use this command!')
+        if(!voiceChannel) return message.channel.send('You must be in a voice channel to use this command!')
+        if(!permissions.has('CONNECT')) return message.channel.send('I do not have voice permissions!')
+        if(!permissions.has('SPEAK')) return message.channel.send('I do not have voice permissions!')
         if(!args[0]) return message.channel.send('Please specify what you want to do (play, pause, skip, etc.)')
+        const serverQueue = queue.get(message.guild.id)
 
         switch(args[0]){
 
             case 'play':
 
-                function play(connection, message){
-                    var server = servers[message.guild.id];
+                const songInfo = await ytdl.getInfo(args[1])
+                const song = {
+                    title: songInfo.videoDetails.title,
+                    url: songInfo.videoDetails.video_url,
+                    thumbnail: songInfo.videoDetails.thumbnail.thumbnails[1].url
+                }
 
-                    server.dispatcher = connection.play(ytdl(server.queue[0], {filter: 'audioonly'}));
-
-                    server.queue.shift();
-
-                    server.dispatcher.on("finish", () =>{
-                        if(server.queue[0]){
-                            play(connection, message);
-                        }else{
-                            connection.disconnect();
-                        }
-                        });
+                if(!serverQueue){
+                    const queueConstruct = {
+                        textChannel: message.channel,
+                        voiceChannel: voiceChannel,
+                        connection: null,
+                        songs: [],
+                        volume: 5,
+                        playing: true
+                    }
+                    queue.set(message.guild.id, queueConstruct)
+        
+                    queueConstruct.songs.push(song)
+        
+                    try {
+                        var connection = await voiceChannel.join()
+                        queueConstruct.connection = connection
+                        play(message.guild, queueConstruct.songs[0])
+                    } catch (error) {
+                        queue.delete(message.guild.id)
+                        return message.channel.send('There was an error connecting to the voice channel!');
+                    }
+                } else{
+                    serverQueue.songs.push(song)
+                    let embed = new discord.MessageEmbed()
+                    .setColor('#007dff')
+                    .setTitle(song.title)
+                    .setDescription('Song added to queue!')
+                    .setThumbnail(song.thumbnail)
+                    .setURL(song.url)
+                    .setAuthor('Cubic | Music', 'https://cdn.discordapp.com/avatars/717580928683212882/4e4a8cb8c44b31a1634d446569f8ad73.png')
+                    
+                    return message.channel.send(embed)
                 }
 
 
-                if(!args[1]) return message.channel.send('Please provide a song link/name!');
-                
-                if(!servers[message.guild.id]) servers[message.guild.id] = {
-                    queue: []
-                }
 
-                var server = servers[message.guild.id];
+            break;
 
-                server.queue.push(args[1]);
-
-                if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
-                    play(connection, message);
-                })
-
+            case 'stop':
+                if(!serverQueue) return message.channel.send('There is nothing playing!')
+                serverQueue.songs = []
+                serverQueue.connection.dispatcher.end()
+                message.channel.send('Stopped playing music!')
 
             break;
 
             case 'skip':
-                var server = servers[message.guild.id];
-                if(server.dispatcher) server.dispatcher.end();
+                if(!serverQueue) return message.channel.send('There is nothing playing!')
+                serverQueue.connection.dispatcher.end()
                 message.channel.send('Skipped the current song!')
+
             break;
 
-            case 'stop':
-                var server = servers[message.guild.id];
-                if(message.guild.voiceConnection){
-                    for(var i = server.queue.length -1; i >=0; i--){
-                        server.queue.splice(i, 1);
-                    }
+        }
 
-                    server.dispatcher.end();
-                    message.channel.send('Stopped the queue.')
-                }
+        function play(guild, song){
+            const serverQueue = queue.get(guild.id)
 
-                if(message.guild.connection) message.guild.voiceConnection.disconnect();
-            break;
+            if(!song){
+                serverQueue.voiceChannel.leave()
+                queue.delete(guild.id)
+                return
+            }
+
+            const dispatcher = serverQueue.connection.play(ytdl(song.url))
+            .on('finish', () => {
+                serverQueue.songs.shift()
+                play(guild, serverQueue.songs[0])
+            })
+            .on('error', error => {
+                console.log(error)
+            })
+            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
 
         }
         
