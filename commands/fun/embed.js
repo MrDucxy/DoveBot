6be71f227ef9fdea6e3c157e51ec22b9edd4602a
Ducxy <dkidd5151@gmail.com>
2020-08-30 13:07:49 -0400

    f

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -15,7 +15,7 @@ module.exports = {
         .setAuthor('Cubic | User Embed', 'https://media.giphy.com/media/j3J8QlFC5avvVd1JAj/giphy.gif')
         .setThumbnail(message.author.avatarURL())
         .setTitle(`Embed From: ${message.author.username}`)
-        .setDescription(args.join(' '))
+        .setDescription(args.join(""))
     
         message.reply(embed);
     }
