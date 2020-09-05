3cf1ff1a5f2ba06e49c8a641de6ecb1b2894df6e
Ducxy <dkidd5151@gmail.com>
2020-09-05 13:27:52 -0400

    Update ytdl.js

--- commands/music/ytdl.js
+++ commands/music/ytdl.js
@@ -50,7 +50,7 @@ module.exports = {
 
 
                     try {
-                        const songDir = `${__dirname + song.title.replace(/ /g,"_")}.mp3`
+                        const songDir = `${__dirname}/${song.title.replace(/ /g,"_")}.mp3`
                         ytdl(song.url, { filter: "audioonly"}).pipe(fs.createWriteStream(songDir)).on("finish", async function(){
                            var songAttachment = new discord.MessageAttachment(songDir)
    
