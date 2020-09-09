9d81164d1e62d41f606621e7a8f35ed59bd8b48e
Ducxy <dkidd5151@gmail.com>
2020-09-09 14:03:23 -0400

    Update guildinfo.js

--- commands/info/guildinfo.js
+++ commands/info/guildinfo.js
@@ -8,19 +8,38 @@ module.exports = {
         usage: "$guildinfo",
     run: async (bot, message, args) => {
 
-        let embed = new discord.MessageEmbed()
-        .setColor('#007dff')
-        .setTitle(message.guild.name)
-        .setThumbnail(message.guild.iconURL())
-        .setAuthor('Cubic | Guild Info', 'https://media.giphy.com/media/j3J8QlFC5avvVd1JAj/giphy.gif')
-        .addField('Server Info',
-         `**Name: **${message.guild.name}
-         **ID: **${message.guild.id}
-         **Members: **${message.guild.memberCount}
-         **Owner: **${message.guild.owner.user.username}
-         **Created: **${message.guild.createdAt}`)
+        try {
 
-        message.reply(embed);
+            let embed = new discord.MessageEmbed()
+            .setColor('#007dff')
+            .setTitle(message.guild.name)
+            .setThumbnail(message.guild.iconURL())
+            .setAuthor('Cubic | Guild Info', 'https://media.giphy.com/media/j3J8QlFC5avvVd1JAj/giphy.gif')
+            .addField('Server Info',
+             `**Name: **${message.guild.name}
+             **ID: **${message.guild.id}
+             **Members: **${message.guild.memberCount}
+             **Owner: **${message.guild.owner.user.username}
+             **Created: **${message.guild.createdAt}`)
+    
+            message.reply(embed);
+            
+        } catch (error) {
+
+            let embed = new discord.MessageEmbed()
+            .setColor('#007dff')
+            .setTitle(message.guild.name)
+            .setThumbnail(message.guild.iconURL())
+            .setAuthor('Cubic | Guild Info', 'https://media.giphy.com/media/j3J8QlFC5avvVd1JAj/giphy.gif')
+            .addField('Server Info',
+             `**Name: **${message.guild.name}
+             **ID: **${message.guild.id}
+             **Members: **${message.guild.memberCount}
+             **Created: **${message.guild.createdAt}`)
+    
+            message.reply(embed);
+            
+        }
         
     }
 
