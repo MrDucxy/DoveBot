43e8069fc19271dbbaadade6513915d3c5fca6a2
Ducxy (Name Coming Soon) <59884498+MrDucxy@users.noreply.github.com>
2020-09-19 03:52:07 -0400

    Update whois.js

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -23,7 +23,7 @@ module.exports = {
         
             do {
                 // Assign the font to the context and decrement it so it can be measured again
-                ctx.font = `bold ${fontSize -= 10}px sans-serif`;
+                ctx.font = `bold ${fontSize -= 10}px sans`;
                 // Compare pixel width of the text to the canvas minus the approximate avatar size
             } while (ctx.measureText(text).width > canvas.width - 330);
         
@@ -58,22 +58,22 @@ module.exports = {
 
         //Draw the user's join date
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `30px sans-serif`;
+        ctx.font = `30px sans`;
         ctx.fillText(`Joined: ${moment(target.joinedAt).utc().format('MM/DD/YYYY | h:mm A')}`, 250, 105)
 
         //Draw the user's creation date
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `30px sans-serif`;
+        ctx.font = `30px sans`;
         ctx.fillText(`Created: ${moment(target.user.createdAt).utc().format('MM/DD/YYYY | h:mm A')}`, 250, 140)
 
         //Draw the user's highest role
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `30px sans-serif`;
+        ctx.font = `30px sans`;
         ctx.fillText(`Role: ${target.roles.highest.name}`, 250, 175)
 
         //Draw the user's ID
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `30px sans-serif`;
+        ctx.font = `30px sans`;
         ctx.fillText(`ID: ${target.user.id}`, 250, 210)
 
 
