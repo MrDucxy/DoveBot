6db5f55151116a82e83da54c956ba354c03d5929
Ducxy (Name Coming Soon) <59884498+MrDucxy@users.noreply.github.com>
2020-09-21 18:14:56 -0400

    gfd

--- commands/images/disabled.js
+++ commands/images/disabled.js
@@ -10,7 +10,6 @@ module.exports = {
     usage: "$disabled <mention>",
     run: async (bot, message, args) => {
 
-        if(message.channel.type === 'dm') return;
 
         let target = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author);
 
@@ -19,22 +18,46 @@ module.exports = {
         const canvas = Canvas.createCanvas(512, 512);
         const ctx = canvas.getContext('2d');
 
-        const background = await Canvas.loadImage('https://www.belfastairport.com/media/3821/invisible-disabilityjpg.jpg');
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-
-        ctx.beginPath();
-        // Start the arc to form a circle
-        ctx.arc(303, 62.5, 37.5, 0, Math.PI * 2, true);
-        // Put the pen down
-        ctx.closePath();
-        // Clip off the region you drew on
-        ctx.clip();
-	    const avatar = await Canvas.loadImage(target.user.displayAvatarURL({ format: 'png' }));
-        ctx.drawImage(avatar, 265, 25, 75, 75);
-
-        let attachment = new discord.MessageAttachment(canvas.toBuffer(), 'user_disabled.png')
-        message.channel.send(attachment);
+        if(args[0] === 'var1' || !args[0]){
+            const background = await Canvas.loadImage('https://i.redd.it/pkp2egyez3s21.jpg');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    
+    
+            const avatar = await Canvas.loadImage(target.user.displayAvatarURL({ format: 'png' }));
+            ctx.drawImage(avatar, 350, 300, 125, 125);
+    
+            let attachment = new discord.MessageAttachment(canvas.toBuffer(), 'user_disabled.png')
+            message.channel.send(attachment);
+        } else if(args[0] === 'var2'){
+            const background = await Canvas.loadImage('https://www.belfastairport.com/media/3821/invisible-disabilityjpg.jpg');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    
+            ctx.beginPath();
+            // Start the arc to form a circle
+            ctx.arc(303, 62.5, 37.5, 0, Math.PI * 2, true);
+            // Put the pen down
+            ctx.closePath();
+            // Clip off the region you drew on
+            ctx.clip();
+            const avatar = await Canvas.loadImage(target.user.displayAvatarURL({ format: 'png' }));
+            ctx.drawImage(avatar, 265, 25, 75, 75);
+    
+            let attachment = new discord.MessageAttachment(canvas.toBuffer(), 'user_disabled.png')
+            message.channel.send(attachment);
+        } else{
+            const background = await Canvas.loadImage('https://i.redd.it/pkp2egyez3s21.jpg');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    
+    
+            const avatar = await Canvas.loadImage(target.user.displayAvatarURL({ format: 'png' }));
+            ctx.drawImage(avatar, 350, 300, 125, 125);
+    
+            let attachment = new discord.MessageAttachment(canvas.toBuffer(), 'user_disabled.png')
+            message.channel.send(attachment);
+        }
         
     }
 
--- commands/music/ytdl.js
+++ commands/music/ytdl.js
@@ -51,7 +51,7 @@ module.exports = {
 
                     try {
                         const songDir = `${__dirname}/${song.title.replace(/ /g,"_")}.mp3`
-                        ytdl(song.url, { filter: "audioonly"}).pipe(fs.createWriteStream(songDir)).on("finish", async function(){
+                        ytdl(song.url, { filter: "audioonly", format: "mp3"}).pipe(fs.createWriteStream(songDir)).on("finish", async function(){
                            var songAttachment = new discord.MessageAttachment(songDir)
 
    
